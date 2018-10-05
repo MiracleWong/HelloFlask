c3f5b984915a15ff274fdecc4036f155dc38d90a
MiracleWong <cfwr1991@126.com>
2018-10-05 18:09:01 +0800

    Flask Session

--- demos/http/app.py
+++ demos/http/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, redirect, url_for, abort, make_response, json, jsonify,request
+import os
+from flask import Flask, redirect, url_for, abort, make_response, json, jsonify, request, session
 app = Flask(__name__)
+
+app.secret_key = os.getenv('SECRET_KEY', 'asdfghjkl')
 
 
 # @app.route('/')
 # def hello_world():
 #     return 'Hello World!'
+
 
 @app.route('/')
 @app.route('/hello')
@@ -12,7 +16,13 @@ def hello():
     name = request.args.get('name')
     if name is None:
         name = request.cookies.get('name', 'Human')
-    return '<h1>Hello, %s</h1>' % name
+        response = '<h1>Hello, %s</h1>' % name
+
+        if 'logged_in' in session:
+            response += '[Authenticated]'
+        else:
+            response += '[Not Authenticated]'
+        return response
 
 
 @app.route('/hi')
@@ -48,6 +58,29 @@ def set_cookie(name):
     response = make_response(redirect(url_for('hello')))
     response.set_cookie('name', name)
     return response
+
+
+## 模拟登录
+@app.route('/login')
+def login():
+    session['logged_in'] = True
+    return redirect(url_for('hello'))
+
+
+## 模拟管理后台
+@app.route('/admin')
+def admin():
+    if 'logged_in' not in session:
+        abort(403)
+    return 'Welcome to admin page.'
+
+
+## 模拟管理后台
+@app.route('/logout')
+def logout():
+    if 'logged_in' in session:
+        session.pop('logged_in')
+    return redirect(url_for('hello'))
 
 
 if __name__ == '__main__':
