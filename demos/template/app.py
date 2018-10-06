b5ac734e8d11f0c87322568adb1fe3a27c77ea68
MiracleWong <cfwr1991@126.com>
2018-10-06 11:32:18 +0800

    加入过滤器

--- demos/template/app.py
+++ demos/template/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
 
 app = Flask(__name__)
 
@@ -29,6 +29,33 @@ def hello_world():
 @app.route('/watchlist')
 def watchlist():
     return render_template('watchlist.html', user=user, movies=movies)
+
+
+# 注册模板上下文处理函数
+@app.context_processor
+def inject_info():
+    foo = 'I am foo.'
+    # return {'foo': foo}
+    return dict(foo=foo)  # equal to: return {'foo': foo}
+
+
+# 注册全局模板函数
+@app.template_global()
+def bar():
+    return 'I am bar. '
+
+
+# 内置过滤器
+@app.route('/hello')
+def hello():
+    text = Markup('<h1>Hello, Flask!</h1>')
+    return render_template('index.html', text=text)
+
+
+# 自定义过滤器
+@app.template_filter()
+def musical(s):
+    return s + Markup(' &#9835;')
 
 
 if __name__ == '__main__':
