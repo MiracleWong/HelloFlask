d0cec705a6022e6ba8cd4127bbc1cc24935de242
MiracleWong <cfwr1991@126.com>
2018-10-06 11:09:07 +0800

    Template Basic Use

--- demos/template/app.py
+++ demos/template/app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+user = {
+    'username': 'Miracle Wong',
+    'bio': 'A boy who loves moives and music.'
+}
+
+movies = [
+    {'name': 'My Neighbor Totoro', 'year': '1988'},
+    {'name': 'Three Colours trilogy', 'year': '1993'},
+    {'name': 'Forrest Gump', 'year': '1994'},
+    {'name': 'Perfect Blue', 'year': '1997'},
+    {'name': 'The Matrix', 'year': '1999'},
+    {'name': 'Memento', 'year': '2000'},
+    {'name': 'The Bucket list', 'year': '2007'},
+    {'name': 'Black Swan', 'year': '2010'},
+    {'name': 'Gone Girl', 'year': '2014'},
+    {'name': 'CoCo', 'year': '2017'},
+]
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/watchlist')
+def watchlist():
+    return render_template('watchlist.html', user=user, movies=movies)
 
 
 if __name__ == '__main__':
