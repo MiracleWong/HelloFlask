1245571beae2385abc2ed958e6a362b20fd43940
MiracleWong <cfwr1991@126.com>
2018-10-07 12:40:16 +0800

    Add 404 & 505 Pages

--- demos/template/app.py
+++ demos/template/app.py
@@ -77,5 +77,15 @@ def just_flash():
     return redirect(url_for('hello'))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
+
+
 if __name__ == '__main__':
     app.run()
