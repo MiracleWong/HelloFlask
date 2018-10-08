3418d2afea83f0c56260300bcb96ebd01272d52b
MiracleWong <cfwr1991@126.com>
2018-10-08 20:15:27 +0800

    Add the demo form

---
+++ demos/form/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from forms import LoginForm
+import os
+
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY', 'secret string')
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/basic')
+def basic():
+    form = LoginForm()
+    return render_template('basic.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run()
