8b517435d0f54ce9ed2b90761c46f28d76641c10
MiracleWong <cfwr1991@126.com>
2018-10-21 10:41:12 +0800

    5.3.1 Flask-SQLAlchemy 连接数据库服务器

--- demos/database/app.py
+++ demos/database/app.py
@@ -1,6 +1,26 @@
+import os, sys
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# sqlite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(app.root_path, 'data.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
